32770782427c9497a2c6c5bb9ffd0109388e0a1f
miyagi0526 <169062105+miyagi0526@users.noreply.github.com>
2024-06-22 01:40:42 +0000

    [Finish] headerのレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  get 'homes/top'
+  devise_for :users
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
+
+  resources :users, only: [:show, :edit, :index]
+
+  root to: 'homes#top'
+  get "/" => "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
+
+
 end
